4c4a5c15f55c48d583bf71140df4ce425cd22d0b
khoa0319 <khoa0319@gmail.com>
2019-05-17 04:14:56 +0700

    update offline.js

---
+++ public/offline.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-restricted-globals */
+// The SW will be shutdown when not in use to save memory,
+// be aware that any global state is likely to disappear
+console.log("SW startup");
+
+self.addEventListener('install', function(event) {
+  console.log("SW installed");
+});
+
+self.addEventListener('activate', function(event) {
+  console.log("SW activated");
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log("Caught a fetch!");
+  event.respondWith(new Response("Hello world!"));
+});
