00f74afd11aeb22bd4af83656b389b7d4801019d
khoa0319 <khoa0319@gmail.com>
2019-05-17 13:43:04 +0700

    change precached

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-restricted-globals */
 const staticCacheName = 'our-first-cache';
-const fileToCache = ['index.html']
+const PRECACHE_URLS = ['/asset.js','/asset.css'];
 self.addEventListener('install', event => {
   console.log('attemping to install service worker and cache statis asset');
   event.waitUntil(
     caches.open(staticCacheName)
     .then(cache => {
-      cache.addAll(fileToCache);
+      cache.addAll(PRECACHE_URLS);
     })
   )
 })
